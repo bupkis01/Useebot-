323eee44fe9ceeaa71e35270496bc24e98589360
bupkis01 <ahammedfavas2004@gmail.com>
2026-04-27 22:34:51 +0530

    Create emoji_ids.py

---
+++ data/emoji_ids.py
@@ -0,0 +1,74 @@
+# Map normal emoji used in messages → (premium emoji, premium ID)
+LEAGUE_EMOJIS = {
+    "🏆": ("💙", 6231268265344111877),  # UEFA Champions League
+    "🏆": ("💙", 6231268265344111877),  # UCL Qualifying
+    "🥇": ("🧡", 6230916309954074577),  # UEFA Europa League
+    "🌟": ("🧡", 6230916309954074577),  # Europa League Qualifying
+    "🏹": ("💚", 6230936723933632548),  # UEFA Conference League
+    "🏹": ("💚", 6230936723933632548),  # Conference League Qualifying
+    "💃": ("🩵", 6230823998221982334),  # UEFA Woman's Champions League
+    "🥈": ("🤡", 6235274099736713004),  # Euro Cup
+    "😁": ("🩵", 6231270309748545580),  # UEFA Super Cup
+    "🎯": ("🤡", 6235274099736713004),  # Euro Nations League
+    "🔥": ("👑", 6230954758501308669),  # Premier League
+    "🍺": ("🐮", 6233033934989432804),  # Carabao Cup
+    "⚽": ("🛫", 6231041731589052252),  # FA Cup
+    "🌸": ("💃", 6230730153186564171),  # Woman's Super League
+    "🥅": ("❤️", 6231047418125753255),  # La Liga
+    "🎀": ("💜", 6231196822858110534),  # Copa de la Reina
+    "🦁": ("💜", 6230730239085909855),  # Copa del Rey
+    "🥰": ("✊", 6230991390277376256),  # Bundesliga
+    "⚡": ("💚", 6233423084796254392),  # DFB-Pokal---
+    "😇": ("💙", 6233506209593301382),  # Serie A
+    "🎩": ("💚", 6235585008124302778),  # Coppa Italia
+    "🥖": ("💙", 6232987957364528428),  # Ligue 1
+    "🍷": ("💙", 6233034287176752114),  # Coupe de France
+    "🌴": ("🤹", 6235311341398136781),  # Campeonato Paulista
+    "🪄": ("🤹", 6233101073918203917),  # Brasileirao Serie A
+    "💰": ("🐪", 6235369705708722478),  # Saudi Pro League
+    "🍣": ("🎎", 6233343237059254815),  # J-League
+    "🕌": ("🦦", 6233349877078695015),  # I-League
+    "🐉": ("🈺", 6233367430610032857),  # Chinese Super League
+    "🪔": ("🧡", 6233469062421157491),  # Indian Super League
+    "🎊": ("🐮", 6233385533897186054),  # Indian Super Cup
+    "💎": ("💜", 6235571534811895876),  # AFC Champions League Elite
+    "🔮": ("💙", 6237723253297649538),  # AFC Champions League Two
+    "🌊": ("💙", 6233152450316998102),  # Liga 1 Indonesia
+    "🦅": ("🌚", 6235494723616774816),  # CAF Champions League
+    "🐘": ("🖤", 6237781819471698189),  # CAF Confederation Cup
+    "🌞": ("🙈", 6235534245905832056),  # Africa Cup of Nations
+    "🎭": ("🙈", 6235534245905832056),  # Africa Nations Championship
+    "🗽": ("🍔", 6233375857335869636),  # Concacaf Champions Cup
+    "💩": ("🌈", 6233192436462523920),  # MLS
+    "🌏": ("🏆", 6235533872243677101),  # World Cup 2026
+    "🥳": ("💤", 6235645459788994896),  # Club World Cup
+    "🪐": ("🏆", 6235533872243677101),  # W-World Cup 2027
+    "🤭": ("🤡", 6235274099736713004),  # World Cup Qualifying UEFA 
+    "😎": ("🤓", 6237665060785756354),  # World Cup Qualifying AFC
+    "😬": ("👻", 6237883644556351502),  # World Cup Qualifying OFC
+    "🌵": ("🌝", 6235446637162928203),  # World Cup Qualifying CONCACAF
+    "🌞": ("🙈", 6235534245905832056),  # World Cup Qualifying CAF
+    "🌋": ("🧬", 6237952205119297083),  # World Cup Qualifying CONMEBOL
+    "🌱": ("👱", 6237809410341608064),  # U-20 World Cup
+    "🌎": ("🧬", 6237952205119297083),  # Copa América
+    "🀄": ("🤓", 6237665060785756354),  # Asian Cup
+    "🈸": ("🤓", 6237665060785756354),  # AFC Asian Cup Qualifying
+    "🏋️‍♂️": ("🏆", 6235533872243677101),  # Men's Olympic
+    "🥹": ("🤍", 6231218035701585589),  # EFL Trophy
+    "🤸‍♀️": ("🏆", 6235533872243677101),  # Woman's Olympic
+    "🫂": ("🤝", 6237485977829383602),  # Friendly
+    "🫂": ("🤝", 6237485977829383602),  # Friendly
+}
+
+# Header emojis
+HEADER_EMOJI_IDS = [
+    4969746438188696430,
+    5287539396361346240,
+    5287380327952570258,
+    5289754946651052290,
+    5289706310441392965,
+    5287704829911652010,
+    5287235720698682838,
+    5287418995543140678,
+    5287307803134813043
+]
