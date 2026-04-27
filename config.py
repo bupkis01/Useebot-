1be62a16f0d67144f812c5b5b3f0f6c10706b02c
bupkis01 <ahammedfavas2004@gmail.com>
2026-04-27 22:21:24 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+api_id = ********             # Your API ID
+api_hash = "********************"  # Your API Hash
+session_name = "userbot"    # Session file name
+group = "ney01gftr"         # Target group username or chat ID
